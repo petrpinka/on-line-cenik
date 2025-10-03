22def81bcd529bc56dda289a4083b7153c5ac222
petrpinka <petr.pinka@gmail.com>
2025-10-03 22:43:36 +0200

    Update cenik-test.js

--- cenik-test.js
+++ cenik-test.js
@@ -16,7 +16,7 @@
   fetch(URL_JSON)
     .then(function(r){ return r.json(); })
     .then(function(data){
-      var table = document.getElementById("cenik-test");
+      var table = document.getElementById("cenik-curak");
       if (!table) return;
 
       table.style.tableLayout = "fixed";
